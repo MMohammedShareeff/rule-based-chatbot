801437ca1fabe529b8351d606949059e06684067
mohammed shareef <mohammadshariffethi@gmail.com>
2026-03-17 23:51:03 +0200

    handled dotenv and requirement file changes

--- main.py
+++ main.py
@@ -3,25 +3,36 @@ import os
 from flask import Flask, render_template, request, jsonify
 from google.cloud import dialogflow_v2 as dialogflow
 from dotenv import load_dotenv
+from google.oauth2 import service_account
+import json
+
+load_dotenv()
 
 app = Flask(__name__)
 
 # PROJECT_ID = os.getenv("PROJECT_ID")
 PROJECT_ID = "ppu-helper-r9hm"
-GOOGLE_API_KEY = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 LANGUAGE_CODE = "ar"  # Arabic
 
 
 def get_dialogflow_response(project_id, session_id, text):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.types.TextInput(text=text, language_code=LANGUAGE_CODE)
-    query_input = dialogflow.types.QueryInput(text=text_input)
-
     try:
+        cred_value = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        
+        if cred_value and cred_value.startswith("{"):
+            info = json.loads(cred_value)
+            credentials = service_account.Credentials.from_service_account_info(info)
+            session_client = dialogflow.SessionsClient(credentials=credentials)
+        else:
+            session_client = dialogflow.SessionsClient()
+
+        session = session_client.session_path(project_id, session_id)
+        text_input = dialogflow.types.TextInput(text=text, language_code=LANGUAGE_CODE)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
         response = session_client.detect_intent(session=session, query_input=query_input)
         return response.query_result.fulfillment_text
+
     except Exception as e:
         print(f"Error connecting to Dialogflow: {e}")
         return "عذراً، حدث خطأ في الاتصال بالمساعد الذكي."
